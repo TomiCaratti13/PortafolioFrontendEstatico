4e1bb47c301562341b7d0ca667441f2facba206a
TomiCaratti13 <tomircaratti@gmail.com>
2023-02-09 16:22:05 -0300

    commit, avances

--- assets/js/acciones.js
+++ assets/js/acciones.js
@@ -1,9 +1,9 @@
-// Al hacer scroll se ejecuta esta funcion(para poner translucida la navbar)
+// Al hacer scroll se ejecuta esta funcion(para poner obscura la navbar)
 
 window.addEventListener("scroll", function(){
 
     var nav = document.querySelector("nav");
-    nav.classList.toggle("translucido",window.scrollY>0);
+    nav.classList.toggle("obscuro",window.scrollY>0);
     
     })
 
