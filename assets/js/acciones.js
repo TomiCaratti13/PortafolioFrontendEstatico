499888dff8bcb3cc4fd424d7489dec7ef7ac8244
TomiCaratti13 <tomircaratti@gmail.com>
2023-02-10 17:47:12 -0300

    estructura cmpleta, faltan estilos y datos

--- assets/js/acciones.js
+++
@@ -1,9 +0,0 @@
-// Al hacer scroll se ejecuta esta funcion(para poner obscura la navbar)
-
-window.addEventListener("scroll", function(){
-
-    var nav = document.querySelector("nav");
-    nav.classList.toggle("obscuro",window.scrollY>0);
-    
-    })
-
